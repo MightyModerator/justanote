60ccdb8b81e66d28b410ade90c1778289b7f9c88
Sportfreunderik <116436031+Sportfreunderik@users.noreply.github.com>
2024-01-25 14:58:26 +0100

    New Layout edit Activity Fade in Animation for Main Activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,12 @@ package com.example.myapplication
 
 // Import statements to include Android specific functionalities.
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 // Declaration of MainActivity class which inherits from AppCompatActivity.
@@ -21,6 +21,13 @@ class MainActivity : AppCompatActivity() {
         // The layout file is defined in the project res/layout/activity_main.xml.
         setContentView(R.layout.activity_main)
 
+        // Find the ImageView with the ID 'ivIcon' and assign it to the variable ivIcon.
+        val ivIcon = findViewById<ImageView>(R.id.ivIcon)
+        // Load the fade_in animation defined in res/anim/fade_in.xml and assign it to the variable rotation.
+        val fade = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        // Start the animation on the ivIcon ImageView.
+        ivIcon.startAnimation(fade)
+
         // Find the Button view with the ID 'btnLogin' and assign it to the variable btnLogin.
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         // Set an onClickListener on the button to define what should happen when the button is clicked.
@@ -28,12 +35,6 @@ class MainActivity : AppCompatActivity() {
             // Display a Toast message. Toasts provide simple feedback about an operation in a small popup.
             Toast.makeText(this, getString(R.string.signed_in), Toast.LENGTH_LONG).show()
 
-            // Find the ImageView with the ID 'ivIcon' and assign it to the variable ivIcon.
-            val ivIcon = findViewById<ImageView>(R.id.ivIcon)
-            // Load the rotation animation defined in res/anim/rotate.xml and assign it to the variable rotation.
-            val rotation = AnimationUtils.loadAnimation(this, R.anim.rotate)
-            // Start the animation on the ivIcon ImageView.
-            ivIcon.startAnimation(rotation)
 
             // Create an Intent to start ListActivity, which is another activity in our application.
             val intent = Intent(this, ListActivity::class.java)
